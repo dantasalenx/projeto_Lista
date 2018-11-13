47387b999a1f756c19daf8e86379d07b8329c059
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-11-12 20:20:06 -0400

    lista

---
+++ projeto_Lista/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace projeto_Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+            List<string> listacompleta = new List<string>();
+
+            var valor = "";
+            do
+            {
+                Console.WriteLine("Informe um nome para add na lista: ");
+                valor = Console.ReadLine().TrimEnd().ToUpper();
+                if (valor != "valor") lista.Add(valor);
+            } while (valor != "sair".ToUpper());
+
+            int quantidade = lista.Count;
+
+            listacompleta = lista.ToList();
+
+
+            Console.WriteLine("Informe o nome a ser excluido: ");
+            lista.Remove(Console.ReadLine().ToUpper());
+
+
+            foreach(var e in listacompleta){
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("-------------");
+            foreach (var e in lista)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("Quantidade inicial: " + quantidade);
+            Console.WriteLine("Quantidade final: " + lista.Count);
+            }
+        }
+    }
